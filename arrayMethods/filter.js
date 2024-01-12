2f068fb266a609c2a40ed06257ee0808cd6920c8
azimbeg <azimbeg28@gmail.com>
2024-01-12 21:22:40 +0530

    update

--- arrayMethods/filter.js
+++ arrayMethods/filter.js
@@ -1,13 +1,33 @@
 //filter even numbers 
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let result = numbers.filter(checkNumber);
+// let result = numbers.filter(function(even){
+//     if(even%2 ==0 || even === 0){
+//         // console.log(even);
+//       return even;
+//     }
+// });
 
-function checkNumber(even){
-    if(even%2 ==0 || even === 0){
-      return even;
-    }
-}
+// console.log(result);
 
-console.log(result);
+// filter odd numbers;
+
+// let result2 = numbers.filter(function(odd){
+//     if(odd%2!=0){
+//         return odd;
+//     }
+// }) ;
+
+// console.log(result2);
+
+
+// Given an array of names, filter out the names that start with the letter "S".
+
+const names = ["Sam", "Sarah", "John", "Susan", "Mike"];
+
+
+let stringResult = names.filter((name)=>name.startsWith("S"));
+
+console.log(stringResult);
+
