768b19acacd35c9618e4f8fca58dff453a20e939
azimbeg <azimbeg28@gmail.com>
2024-01-18 22:15:09 +0530

    update

---
+++ stringMethods/slice.js
@@ -0,0 +1,25 @@
+// let text = "Apple, Banana, Kiwi";
+// let newtext = "Mango";
+
+// let sliceString = text.slice(-8,-3);
+// let substrstring = text.substr(5,6);
+// let upperCaseStrong = text.toUpperCase()
+// let lowerCaseStrong = text.toLowerCase()
+// console.log(`upper case String = ${upperCaseStrong} lower case string = ${lowerCaseStrong}` );
+// console.log( text + newtext);
+
+// let text = "      hello azim      ";
+// console.log(text);
+// text = text.trim();
+// console.log(text);
+
+// let string1 = "42";
+// let newString = string1.padStart(5," ");
+// console.log(newString);
+
+const func = (data1,data2)=>(
+    data1 + data2
+)
+
+console.log(func(1,2))
+
